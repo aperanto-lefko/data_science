3ee9b820938ff0821802a158eb6540c1ae770f0e
aperanto-lefko <polarster@yandex.ru>
2025-04-17 09:21:31 +0300

    Создание проекта bike

---
+++ bike/BikeSharingPrediction/BikeSharingPrediction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BikeSharingPrediction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
